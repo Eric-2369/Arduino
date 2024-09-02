2de0d0432b03d6c22f10e6d2d312e85673bc420c
Eric-2369 <admin@eri.cx>
2024-09-02 13:56:00 +0800

    feat: implement formaldehyde-sensor

--- src/formaldehyde-sensor/thingProperties.h
+++ src/formaldehyde-sensor/thingProperties.h
@@ -3,14 +3,13 @@
 #include <ArduinoIoTCloud.h>
 #include <Arduino_ConnectionHandler.h>
 
-const char SSID[]     = SECRET_SSID;    // Network SSID (name)
-const char PASS[]     = SECRET_OPTIONAL_PASS;    // Network password (use for WPA, or use as key for WEP)
+const char SSID[] = SECRET_SSID;           // Network SSID (name)
+const char PASS[] = SECRET_OPTIONAL_PASS;  // Network password (use for WPA, or use as key for WEP)
 
-
+CloudCounter cloud_wzCH2OConcentration;
 
 void initProperties(){
-
-
+  ArduinoCloud.addProperty(cloud_wzCH2OConcentration, READ, 1 * SECONDS, NULL);
 }
 
 WiFiConnectionHandler ArduinoIoTPreferredConnection(SSID, PASS);
